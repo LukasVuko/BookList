fad85a257d27cb7c6c7f40a3c56df8cedfa50d76
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-10-16 15:28:07 -0400

    Add to book to user interface complete. To be added: Validation

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ function Book(title, author, isbn) {
 
 // UI Constructor
 
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('book-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td><a href='#' class='delete'>X</a></td>
+                    `;
+  list.appendChild(row);
+};
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+};
+
 // Event listeners
 
 document.getElementById('book-form').addEventListener('submit', addBook);
@@ -18,7 +39,13 @@ function addBook(e) {
     author = document.getElementById('author').value,
     isbn = document.getElementById('isbn').value;
 
+  // Instantiate a book
   const book = new Book(title, author, isbn);
 
-  console.log(book);
+  // Instantiate a UI component
+  const ui = new UI();
+
+  ui.addBookToList(book);
+
+  ui.clearFields();
 }
